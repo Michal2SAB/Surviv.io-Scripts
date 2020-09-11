7a6ece3970afcc75c818c8705a2b4c2c7112ddf1
Michał <38110165+Michal2SAB@users.noreply.github.com>
2020-09-11 21:28:58 +0200

    Update ShowHealth.js

--- Cool Mods/ShowHealth.js
+++ Cool Mods/ShowHealth.js
@@ -1,24 +1,50 @@
+// ==UserScript==
+// @name         Surviv Health Mod
+// @namespace    https://github.com/Michal2SAB
+// @version      1.0
+// @description  Shows you the health number, that's all...
+// @author       Michal2SAB
+// @match        *://surviv.io/*
+// @match        *://surviv2.io/*
+// @match        *://2dbattleroyale.com/*
+// @match        *://2dbattleroyale.org/*
+// @match        *://piearesquared.info/*
+// @match        *://thecircleisclosing.com/*
+// @match        *://archimedesofsyracuse.info/*
+// @match        *://secantsecant.com/*
+// @match        *://parmainitiative.com/*
+// @match        *://nevelskoygroup.com/*
+// @match        *://kugahi.com/*
+// @match        *://chandlertallowmd.com/*
+// @match        *://ot38.club/*
+// @match        *://kugaheavyindustry.com/*
+// @match        *://drchandlertallow.com/*
+// @match        *://rarepotato.com/*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
 (function() {
     'use strict';
-    var o = document.createElement("a");
-    o.setAttribute("id","my_Heart");
-    o.style.color = "green";
-    o.style.fontSize = "25px";
-    o.style.display = "block";
-    document.getElementById("ui-boost-counter").parentNode.appendChild(o);
-    var reference = document.getElementById('ui-boost-counter');
-    reference.parentNode.insertBefore(o, reference);
+    var lastHP = 0
+    var health = $("<span></span>")
+    health.css({
+        display: "block",
+        position: "fixed",
+        "z-index": "2",
+        margin: "6px 0 0 0",
+        right: "12px",
+        color: "blue",
+        "font-weight": "bold",
+        "font-size": "large",
+    })
+    $("#ui-health-container").append(health)
 
     setInterval(function(){
-        o.innerHTML ="HP: " + Math.round(document.getElementById("ui-health-actual").style.width.slice(0,-1));
-        if(document.getElementById("game-area-wrapper").style.display == "block"){
-            if(o.innerHTML.slice(5,8) < 25){
-                o.style.color = "red";
-            } else {
-                if(o.innerHTML.slice(5,8) > 25){
-                    o.style.color = "green";
-                }
-            }       
+        var hp = Math.round(document.getElementById("ui-health-actual").style.width.slice(0,-1))
+        if(lastHP !== hp){
+            lastHP = hp
+            health.html(Math.ceil(hp))
         }
-    },500);
+    })
 })();
